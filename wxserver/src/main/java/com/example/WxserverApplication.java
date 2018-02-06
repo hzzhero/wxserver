a805f0354a19fe18941732aa9a41f7bdadad307a
admin <admin@He>
2018-02-06 18:39:07 +0800

    hzz

--- wxserver/src/main/java/com/example/WxserverApplication.java
+++ wxserver/src/main/java/com/example/WxserverApplication.java
@@ -3,6 +3,11 @@ package com.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 程序入口
+ * @author hzz
+ *
+ */
 @SpringBootApplication
 public class WxserverApplication {
 
